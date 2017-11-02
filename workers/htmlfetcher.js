11b3e95e01b95254a6dcefea7b354c0884043c65
aricalves <aric.alves2012@gmail.com>
2017-11-02 15:47:02 -0700

    Able to download sites and archive them

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,3 +1,13 @@
-var request = require('request');
-var fs = require('fs');
-var helpers = require('../helpers/archive-helpers');
+const request = require('request');
+const fs = require('fs');
+const helpers = require('../helpers/archive-helpers');
+
+const fetcher = () => {
+  helpers.readListOfUrls(urls => {
+    helpers.downloadUrls(urls);
+  });
+  //fs.writeFile(helpers.paths.list, '', () => { console.log('File cleared'); });
+};
+
+fetcher();
+
